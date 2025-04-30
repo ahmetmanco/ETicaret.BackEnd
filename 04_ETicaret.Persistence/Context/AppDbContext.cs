67caa83cc56206180d54072bcdfcdf1ade226f0f
ahmetmanco <114330172+ahmetmanco@users.noreply.github.com>
2025-05-01 01:06:52 +0300

    Created ProductVM and ProductController

--- 04_ETicaret.Persistence/Context/AppDbContext.cs
+++ 04_ETicaret.Persistence/Context/AppDbContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using _01_ETicaret.Domain.Base;
 using _01_ETicaret.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -28,6 +29,22 @@ namespace _04_ETicaret.Persistence_.Context
                 foreingKey.DeleteBehavior = DeleteBehavior.Restrict;
             }
             base.OnModelCreating(builder);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var datas = ChangeTracker.Entries<BaseEntity>();
+
+            foreach (var data in datas)
+            {
+                _ = data.State switch
+                {
+                    EntityState.Added => data.Entity.CreatedDate = DateTime.Now,
+                    EntityState.Modified => data.Entity.UpdateDate = DateTime.Now,
+                    EntityState.Deleted => data.Entity.DeletedDate = DateTime.Now,
+                };
+            }
+            return await base.SaveChangesAsync(cancellationToken);
         }
     }
 }
